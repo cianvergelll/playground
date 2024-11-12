69118c4c355a15cef25c9becaae83bcd42980dd0
Ian <cianvergelll@gmail.com>
2024-11-12 20:37:52 +0800

    getShape - Encapsulation

--- playground/index.js
+++ playground/index.js
@@ -1,25 +1,3 @@
-let baseSalary = 30_000;
-let overTime = 10;
-let rate = 20;
-
-function getWage(baseSalary, overTime, rate) {
-    return baseSalary + (overTime * rate);
-}
-
-console.log("Wage from function:", getWage(baseSalary, overTime, rate));
-
-let employee = {
-    baseSalary: 30_000,
-    overtime: 10,
-    rate: 20,
-    getWage: function() {
-        return this.baseSalary + (this.overtime * this.rate);
-    }
-};
-
-console.log("Wage from employee object:", employee.getWage());
-
-
 function getArea(height, width) {
     return height * width;
 }

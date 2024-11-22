934c5cfeb29871830f7fcd3b06a0ea2e17458957
Ian <cianvergelll@gmail.com>
2024-11-22 14:22:32 +0800

    first project pyramid

---
+++ playground/pyramid.js
@@ -0,0 +1,16 @@
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
